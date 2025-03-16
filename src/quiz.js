261791dcbb511dff5ec1d488ae1c6bb644c185d4
hridikabanik02 <109242454+hridikabanik02@users.noreply.github.com>
2025-03-16 14:44:59 -0600

    changed the API to generate music category questions

--- src/quiz.js
+++ src/quiz.js
@@ -5,7 +5,7 @@ let score = 0;
 
 // Fetch Questions from Open Trivia DB (Music Category)
 async function fetchQuestions() {
-    const response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
+    const response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple');
     const data = await response.json();
     return data.results.map(q => ({
         question: q.question,
